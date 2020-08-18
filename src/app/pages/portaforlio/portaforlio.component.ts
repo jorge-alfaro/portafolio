d54adfbd9a2ed735c6271c6cc0e668a9be4bfb02
Jorge M. Alfaro Che <jorgm.alfarc@gmail.com>
2020-08-18 17:33:03 -0500

    Template listo sin rutas

---
+++ src/app/pages/portaforlio/portaforlio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portaforlio',
+  templateUrl: './portaforlio.component.html',
+  styleUrls: ['./portaforlio.component.css']
+})
+export class PortaforlioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
