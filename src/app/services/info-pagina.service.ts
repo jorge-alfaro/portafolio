b2164f947063eb23d7945ba5d35fd63d279a19a7
Jorge M. Alfaro Che <jorgm.alfarc@gmail.com>
2020-08-20 02:18:32 -0500

    Seccion de productos

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -2,28 +2,42 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
 
-  info: InfoPagina = {}
-    loaded = false;
+  info: InfoPagina = {};
+  loaded = false;
+  equipo: any [] = []
 
 
-  constructor(private http: HttpClient) {
-   // console.log('Servicio de infoPagina Listo');
 
-    // Leer el archivo JSON
+  constructor( private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+
+  private cargarInfo(){
     this.http.get('assets/data/data-page.json')
       .subscribe( (resp: InfoPagina)=> {
 
       this.loaded = true;
       this.info = resp;
-      console.log(resp);
-
 
 
     });
+  }
+
+  private cargarEquipo(){
+    this.http.get('https://angular-p-one.firebaseio.com/equipo.json')
+    .subscribe((resp: any [])=>{
+
+      this.equipo = resp;
+      // console.log(resp);
+
+    })
   }
 }
